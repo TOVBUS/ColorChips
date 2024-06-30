da80e1c17b4a7cf701f841ad384b04be0ebee0f5
suye0ng <rlatndud456@naver.com>
2024-06-30 17:56:52 +0900

    ✨ :: [HW]CustomTabView

---
+++ 3_ksy/CustomTabView/CustomTabView/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  CustomTabView
+//
+//  Created by 김수영 on 6/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var currentTab: Tab = .home
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            TabView(selection: $currentTab) {
+                Text("홈뷰")
+                    .tag(Tab.home)
+                Text("게시판뷰")
+                    .tag(Tab.forum)
+                Text("스터디뷰")
+                    .tag(Tab.study)
+                Text("프로필뷰")
+                    .tag(Tab.profile)
+                
+            }
+            
+            CustomTab(currentTab: $currentTab)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 3_ksy/CustomTabView/CustomTabView/CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewApp.swift
+//  CustomTabView
+//
+//  Created by 김수영 on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
