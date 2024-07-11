b4d30837cf1d0f740fb53b16824215990c9a099e
uk10105 <lykalso33@naver.com>
2024-07-11 15:21:56 +0900

    ✏️ :: member 모듈 로직 주석 추가 #KAN-57

--- Backend/febird-api/src/member/entities/member.entity.ts
+++ Backend/febird-api/src/member/entities/member.entity.ts
@@ -1,4 +1,5 @@
 export class Member {}
+// 아이디 id : uuid
 // 닉네임 nickname : string
 // 생년월일 birth : date
 // 성별 gender : string
--- Backend/febird-api/src/member/member.controller.ts
+++ Backend/febird-api/src/member/member.controller.ts
@@ -7,3 +7,9 @@ import { MemberService } from './member.service';
 export class MemberController {
   constructor(private readonly memberService: MemberService) {}
 }
+// 회원가입 - 회원 정보를 받아 회원 가입 처리
+// 로그인 - 로그인 정보를 받아 로그인 처리
+// 로그아웃 - 로그아웃 처리
+// 멤버 조회 - 멤버 일련번호에 맞는 멤버 정보를 조회
+// 멤버 수정 - 멤버 일련번호에 맞는 멤버 정보를 수정
+// 회원탈퇴 - 멤버 일련번호에 맞는 멤버 정보를 삭제
