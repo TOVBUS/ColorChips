7d918de5a9a624768fd1744b4c32975cf100b323
HyeS00 <hyesookim1118@gmail.com>
2024-07-15 17:11:48 +0900

    🛠️ :: swiftLint 적용 #KAN-73

--- Frontend-iOS/FebirdApp/FebirdApp/ContentView.swift
+++ Frontend-iOS/FebirdApp/FebirdApp/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
-
+    
     var body: some View {
         NavigationSplitView {
             List {
