090768dfb92142da85df16ac19004cb60a0696d0
doyeonjeong <debby_@kakao.com>
2024-07-15 16:41:24 +0900

    ⚡️ :: updated Font extension

--- Frontend-iOS/FebirdApp/FebirdApp/Sources/Extensions/Font+Extension.swift
+++ Frontend-iOS/FebirdApp/FebirdApp/Sources/Extensions/Font+Extension.swift
@@ -8,36 +8,80 @@
 import SwiftUI
 
 extension Font {
-
-    // IBM Plex Sans KR 폰트 패밀리 : 한글
-    enum IBMPlexSansKR: String {
-        case regular = "IBMPlexSansKR-Regular"
-        case thin = "IBMPlexSansKR-Thin"
-        case extraLight = "IBMPlexSansKR-ExtraLight"
-        case light = "IBMPlexSansKR-Light"
-        case medium = "IBMPlexSansKR-Medium"
-        case semiBold = "IBMPlexSansKR-SemiBold"
-        case bold = "IBMPlexSansKR-Bold"
+    enum AppFont: String {
+        case ibmPlexSansKR = "IBMPlexSansKR"
+        case workSans = "WorkSans"
     }
-
-    // Work Sans 폰트 패밀리 : 영어
-    enum WorkSans: String {
-        case regular = "WorkSans-Regular"
-        case thin = "WorkSans-Thin"
-        case extraLight = "WorkSans-ExtraLight"
-        case light = "WorkSans-Light"
-        case medium = "WorkSans-Medium"
-        case semiBold = "WorkSans-SemiBold"
-        case bold = "WorkSans-Bold"
-        case extraBold = "WorkSans-ExtraBold"
-        case black = "WorkSans-Black"
+    
+    enum AppFontWeight: String {
+        case thin, extraLight, light, regular, medium, semiBold, bold, extraBold, black
+        
+        var uiFont: UIFont.Weight {
+            switch self {
+            case .thin: return .thin
+            case .extraLight: return .ultraLight
+            case .light: return .light
+            case .regular: return .regular
+            case .medium: return .medium
+            case .semiBold: return .semibold
+            case .bold: return .bold
+            case .extraBold: return .heavy
+            case .black: return .black
+            }
+        }
     }
-
-    static func ibmPlexSansKR(_ font: IBMPlexSansKR, size: CGFloat) -> Font {
-        return Font.custom(font.rawValue, size: size)
+    
+    static func app(_ font: AppFont, size: CGFloat, weight: AppFontWeight = .regular) -> Font {
+        let fontName = "\(font.rawValue)-\(weight.rawValue.capitalized)"
+        return Font.custom(fontName, size: size)
     }
+    
+    static func body(english: Bool = false) -> Font {
+        app(english ? .workSans : .ibmPlexSansKR, size: 14)
+    }
+    
+    static func bodyBold(english: Bool = false) -> Font {
+        app(english ? .workSans : .ibmPlexSansKR, size: 14, weight: .semiBold)
+    }
+    
+    static func label(english: Bool = false) -> Font {
+        app(english ? .workSans : .ibmPlexSansKR, size: 12)
+    }
+    
+    static func subheader(english: Bool = false) -> Font {
+        app(english ? .workSans : .ibmPlexSansKR, size: 18, weight: .medium)
+    }
+    
+    static func sectionHeader(english: Bool = false) -> Font {
+        app(english ? .workSans : .ibmPlexSansKR, size: 30, weight: .medium)
+    }
+    
+    static func header(english: Bool = false) -> Font {
+        app(english ? .workSans : .ibmPlexSansKR, size: 36, weight: .medium)
+    }
+}
 
-    static func workSans(_ font: WorkSans, size: CGFloat) -> Font {
-        return Font.custom(font.rawValue, size: size)
+extension View {
+    func appFont(_ style: Font.TextStyle, english: Bool = false) -> some View {
+        AnyView(
+            Group {
+                switch style {
+                case .body:
+                    self.font(.body(english: english)).lineSpacing(6)
+                case .callout:
+                    self.font(.bodyBold(english: english)).lineSpacing(6)
+                case .caption:
+                    self.font(.label(english: english)).lineSpacing(4)
+                case .subheadline:
+                    self.font(.subheader(english: english)).lineSpacing(10)
+                case .title2:
+                    self.font(.sectionHeader(english: english)).lineSpacing(8)
+                case .title:
+                    self.font(.header(english: english)).lineSpacing(4)
+                default:
+                    self
+                }
+            }
+        )
     }
 }
