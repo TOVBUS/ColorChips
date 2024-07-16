ccaf7c984b9d093fdcfdfb12cb898f7bf8cac656
suye0ng <rlatndud456@naver.com>
2024-07-17 02:19:30 +0900

    ➕ :: exercise table, routine table, level table 추가 및 모듈 설정  KAN-96

--- Backend/febird-api/src/level/dto/create-level.dto.ts
+++ Backend/febird-api/src/level/dto/create-level.dto.ts
@@ -1,1 +1,12 @@
-export class CreateLevelDto {}
+import { IsString, IsNotEmpty, IsUrl } from 'class-validator';
+
+export class CreateLevelDto {
+  @IsString()
+  @IsNotEmpty()
+  school_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUrl()
+  reward_url: string;
+}
--- Backend/febird-api/src/routine/dto/create-routine.dto.ts
+++ Backend/febird-api/src/routine/dto/create-routine.dto.ts
@@ -1,1 +1,19 @@
-export class CreateRoutineDto {}
+import { IsString, IsNotEmpty, IsArray } from 'class-validator';
+import { CreateLevelDto } from '../../level/dto/create-level.dto';
+import { CreateExerciseDto } from '../../exercise/dto/create-exercise.dto';
+
+export class CreateRoutineDto {
+  @IsString()
+  @IsNotEmpty()
+  count: string;
+
+  @IsString()
+  @IsNotEmpty()
+  set: string;
+
+  @IsArray()
+  levels: CreateLevelDto[];
+
+  @IsArray()
+  exercises: CreateExerciseDto[];
+}
