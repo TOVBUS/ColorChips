73a9cdd8372cf3f459ab0ed6e0e3a37e983c704b
uk10105 <lykalso33@naver.com>
2024-07-18 16:02:48 +0900

    ➕ :: MealMainView 생성, AI식단추천뷰 생성

---
+++ Frontend-iOS/FebirdApp/FebirdApp/Sources/Meal/Views/AIMealRecomendationView.swift
@@ -0,0 +1,71 @@
+//
+//  AIMealRecomendationView.swift
+//  FebirdApp
+//
+//  Created by 이유경 on 7/18/24.
+//
+
+import SwiftUI
+
+struct AIMealRecomendationView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            VStack(alignment: .leading, spacing: 24) {
+                HStack(alignment: .center, spacing: 16) {
+                    Text("피오추천")
+                        .foregroundStyle(Color.white)
+                        .bold()
+                }
+                .padding(.horizontal, 8)
+                .padding(.vertical, 4)
+                .frame(height: 24, alignment: .center)
+                .background(Color(red: 0.98, green: 0.45, blue: 0.09))
+                .cornerRadius(8)
+                
+                // 추천받은 식단명
+                Text("샐러디 웜랩")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .bold()
+                
+                // 추천받은 식단의 칼로리, 식단 시간대
+                Group {
+                    Label("251Kcal", systemImage: "flame.fill")
+                    Label("오전식단", systemImage: "fork.knife")
+                }
+                .foregroundStyle(Color.white)
+                
+                // 식단챗봇으로 넘어가는 버튼
+                Button(action: {
+                    
+                }, label: {
+                    HStack(alignment: .center, spacing: 8) {
+                        Text("식단추천받기")
+                        Text("+")
+                    }
+                    .foregroundStyle(Color.white)
+                    .bold()
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 12)
+                    .frame(height: 40, alignment: .center)
+                    .cornerRadius(13)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 13)
+                            .inset(by: 0.5)
+                            .stroke(.white, lineWidth: 1)
+                    )
+                })
+            }
+            .padding(0)
+            .frame(width: 188, alignment: .topLeading)
+        }
+        .padding(16)
+        .frame(maxWidth: .infinity, alignment: .topLeading)
+        .background(Color(red: 0.07, green: 0.07, blue: 0.08))
+        .cornerRadius(32)
+    }
+}
+
+#Preview {
+    AIMealRecomendationView()
+}
---
+++ Frontend-iOS/FebirdApp/FebirdApp/Sources/Meal/Views/MealMainView.swift
@@ -0,0 +1,21 @@
+//
+//  MealMainView.swift
+//  FebirdApp
+//
+//  Created by 이유경 on 7/18/24.
+//
+
+import SwiftUI
+
+struct MealMainView: View {
+    var body: some View {
+        VStack {
+            
+        }
+                    
+    }
+}
+
+#Preview {
+    MealMainView()
+}
