b6a619c5980686f8423b9e7cd4d5ea7065c107d5
HyeS00 <hyesookim1118@gmail.com>
2024-07-21 20:53:25 +0900

    🎨 :: 앨범 뷰 UI 구현 #KAN-180

---
+++ Frontend-iOS/FebirdApp/FebirdApp/Sources/Profile/Views/AlbumMainView.swift
@@ -0,0 +1,18 @@
+//
+//  AlbumMainView.swift
+//  FebirdApp
+//
+//  Created by 김혜수 on 7/21/24.
+//
+
+import SwiftUI
+
+struct AlbumMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlbumMainView()
+}
