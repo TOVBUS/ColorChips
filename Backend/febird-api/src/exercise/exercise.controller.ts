b3beca60d1df048ca95c1572697ba055228144b8
suye0ng <rlatndud456@naver.com>
2024-07-21 19:02:50 +0900

    ✨ :: Exercise의 update-dto 삭제, controller, service에 update&remove 로직 삭제 #KAN-134

--- Backend/febird-api/src/exercise/exercise.controller.ts
+++ Backend/febird-api/src/exercise/exercise.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ExerciseService } from './exercise.service';
 import { CreateExerciseDto } from './dto/create-exercise.dto';
-import { UpdateExerciseDto } from './dto/update-exercise.dto';
 
 @Controller('exercise')
 export class ExerciseController {
@@ -14,15 +13,5 @@ export class ExerciseController {
   @Get()
   findAll() {
     return this.exerciseService.findAll();
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateExerciseDto: UpdateExerciseDto) {
-    return this.exerciseService.update(+id, updateExerciseDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.exerciseService.remove(+id);
   }
 }
