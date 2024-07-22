904a13d274da525a93e0a630c4f70dddada2567a
uk10105 <lykalso33@naver.com>
2024-07-22 16:37:45 +0900

    🎨 :: 소셜 아이콘 연결 #KAN-191

--- Frontend-iOS/FebirdApp/FebirdApp/Sources/Login/Views/LoginView.swift
+++ Frontend-iOS/FebirdApp/FebirdApp/Sources/Login/Views/LoginView.swift
@@ -14,27 +14,26 @@ struct LoginView: View {
                 .foregroundColor(.clear)
                 .background(.orange50)
                 .ignoresSafeArea()
-            
+
             VStack(alignment: .center, spacing: 30) {
                 Text("피오피트에 오신것을 환영합니다🎉")
                     .font(.customFont(size: 24, weight: .bold))
-                
+
                     Text("그동안 혼자 운동하다가\n잘못된 자세로 인해 다친적이 있나요?")
                         .multilineTextAlignment(.leading)
                         .padding(.trailing, 100)
                         .font(.customFont(size: 16, weight: .regular))
-                
+
                     Text("또, 3일 이상 해야지! 마음먹고\n금방 포기하게 되지는 않았나요?")
                         .multilineTextAlignment(.trailing)
                         .padding(.leading, 100)
                         .font(.customFont(size: 16, weight: .regular))
-                
-                
+
                 Text("앞으로는 피오코치가 \n올바른 자세로, 꾸준히 할 수 있도록 도와줄게요!")
                     .font(.customFont(size: 18, weight: .bold))
                     .frame(maxWidth: .infinity, maxHeight: 80)
                     .multilineTextAlignment(.center)
-                
+
                 Image("peoLogo")
                     .resizable()
                     .frame(width: 204, height: 204)
@@ -44,7 +43,12 @@ struct LoginView: View {
                     Button(action: {
                         // TODO: 카카오 로그인 연결
                     }, label: {
-                        Text("카카오로 시작하기")
+                        HStack {
+                            Image("kakao-icon")
+                                .resizable()
+                                .frame(width: 24, height: 22)
+                            Text("카카오로 시작하기")
+                        }
                             .foregroundStyle(Color(red: 0.24, green: 0.12, blue: 0.12))
                             .font(.customFont(size: 16, weight: .semibold))
                             .frame(maxWidth: .infinity, maxHeight: 56)
@@ -54,11 +58,15 @@ struct LoginView: View {
                                     .padding(.horizontal, 24)
                             )
                     })
-                    
+
                     Button(action: {
                         // TODO: 애플 로그인 연결
                     }, label: {
-                        Text("애플로 시작하기")
+                        HStack(spacing: 5) {
+                            Image("apple-icon")
+                                .frame(width: 24, height: 29)
+                            Text("애플로 시작하기")
+                        }
                             .frame(maxWidth: .infinity, maxHeight: 56)
                             .foregroundStyle(.white)
                             .font(.customFont(size: 16, weight: .semibold))
