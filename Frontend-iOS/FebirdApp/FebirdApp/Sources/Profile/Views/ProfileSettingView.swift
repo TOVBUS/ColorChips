1572abcadf225b99acd39c07be96762a011e47e2
uk10105 <lykalso33@naver.com>
2024-07-23 17:41:04 +0900

    🎨 :: ProfileSettingView 구현 #KAN-199

--- Frontend-iOS/FebirdApp/FebirdApp/Sources/Profile/Views/ProfileSettingView.swift
+++ Frontend-iOS/FebirdApp/FebirdApp/Sources/Profile/Views/ProfileSettingView.swift
@@ -10,17 +10,55 @@ import SwiftUI
 struct ProfileSettingView: View {
     var body: some View {
         VStack {
-            ZStack {
-                HeaderView()
-                Image("profile01")
-                    .padding(.top, 121)
-                ZStack {
-                    // TODO: 아이콘 이미지 삽입
+            HeaderView()
+                .padding(.top, -100)
+            
+            VStack(spacing: 20) {
+                HStack {
+                    Text("닉네임").font(.customFont(size: 14, weight: .bold))
+                    Spacer()
                 }
+                
+                HStack(spacing: 8) {
+                    Image("personIcon")
+                        .padding()
+                    TextField("어떻게 불러드릴까요?", text: .constant("꽥꽥이"))
+                    Image("pencilIcon")
+                        .padding()
+                }
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(.orange40, lineWidth: 1)
+                        .fill(.gray10)
+                )
+                .padding(.horizontal, 0)
+                
+                OnboardingTextField(question: "나이", placeholder: "17세", unit: "세", inputValue: nil, text: .constant("16"))
+                
+                HStack {
+                    Text("성별").font(.customFont(size: 14, weight: .bold))
+                    Spacer()
+                }
+                
+                OnboardingSelectGenderButton()
+                    .padding(.bottom, 70)
+
+                Button(action: {
+                    //
+                }, label: {
+                    Text("저장하기")
+                        .frame(maxWidth: .infinity, maxHeight: 56)
+                        .foregroundStyle(Color(.white))
+                        .font(.customFont(size: 16, weight: .semibold))
+                        .background(
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(.gray100)
+                        )
+                })
             }
-            .ignoresSafeArea()
-            Spacer()
+            .padding(25)
         }
+        .ignoresSafeArea()
     }
 }
 
@@ -31,7 +69,7 @@ struct HeaderView: View {
                 .frame(height: 185)
                 .foregroundStyle(.gray100)
                 .cornerRadius(34, corners: [.bottomLeft, .bottomRight])
-            .ignoresSafeArea()
+                .ignoresSafeArea()
 
             HStack(alignment: .center, spacing: 12) {
                 Image("Back_button")
@@ -42,8 +80,12 @@ struct HeaderView: View {
                 Spacer()
             }
             .padding(.top, -55)
+            Image("profile01")
+                .padding(.top, 121)
+            ZStack {
+                // TODO: 아이콘 이미지 삽입
 
-
+            }
         }
     }
 }
