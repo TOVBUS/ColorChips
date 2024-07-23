f54ec7f778047ef588eba3714420aa73d16d12aa
uk10105 <lykalso33@naver.com>
2024-07-23 23:58:17 +0900

    🎨 :: ExerciseRecordView 상단 수정 #KAN-188

--- Frontend-iOS/FebirdApp/FebirdApp/Sources/Exercise/Views/ExerciseRecordView.swift
+++ Frontend-iOS/FebirdApp/FebirdApp/Sources/Exercise/Views/ExerciseRecordView.swift
@@ -10,11 +10,20 @@ import SwiftUI
 struct ExerciseRecordView: View {
     var body: some View {
         VStack {
-            Rectangle()
-                .frame(height: 160)
-                .foregroundStyle(Color.gray100)
-                .cornerRadius(34, corners: [.bottomLeft, .bottomRight])
-                .ignoresSafeArea()
+            ZStack {
+                Rectangle()
+                    .frame(height: 160)
+                    .foregroundStyle(Color.gray100)
+                    .cornerRadius(34, corners: [.bottomLeft, .bottomRight])
+                    .ignoresSafeArea()
+                    .padding(.bottom, -60)
+                HStack {
+                    // TODO: 아이콘 이미지 삽입
+                    Text("현재기록")
+                        .font(.customFont(size: 20, weight: .bold))
+                        .foregroundStyle(.white)
+                }
+            }
             ExerciseRecordListView()
         }
     }
