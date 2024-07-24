2f4b6863042e26d7cbdcbe4220f676e8f9d4211d
이유경 <a260@iyugyeong-ui-MacBookPro.local>
2024-07-25 01:51:40 +0900

    🎨 :: MealMainView 코드리뷰 피드백 내용 수정 #KAN-205

--- Frontend-iOS/FebirdApp/FebirdApp/Sources/Meal/Views/MealMainView.swift
+++ Frontend-iOS/FebirdApp/FebirdApp/Sources/Meal/Views/MealMainView.swift
@@ -9,36 +9,34 @@ import SwiftUI
 
 struct MealMainView: View {
     let meals = ["아침", "점심", "저녁"]
+    
     var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundStyle(Color.white)
-                .ignoresSafeArea()
-            VStack(spacing: 24) {
-                MealMainheaderView()
-                ScrollView {
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("AI 식단추천")
-                            .font(.customFont(size: 20, weight: .bold))
-                            .foregroundStyle(Color.gray100)
-                            .padding(.leading, 20)
-                        AIMealRecomendationView()
-                        Text("식단 기록")
-                            .padding(.leading, 20)
-                            .foregroundStyle(Color.gray100)
-                            .font(.customFont(size: 20, weight: .bold))
-                        ForEach(meals, id: \.self) { meal in
-                            MemoRow(mealTime: meal)
-                        }
-                        .padding(.leading, 20)
-                        .foregroundStyle(Color.gray100)
+        VStack(spacing: 24) {
+            MealMainheaderView()
+            ScrollView {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("AI 식단추천")
+                        .font(.customFont(size: 20, weight: .bold))
+                        .foregroundStyle(.gray100)
+                    
+                    AIMealRecomendationView()
+                        .padding(.bottom, 20)
+                    
+                    Text("식단 기록")
+                        .foregroundStyle(.gray100)
+                        .font(.customFont(size: 20, weight: .bold))
+                    
+                    ForEach(meals, id: \.self) { meal in
+                        MemoRow(mealTime: meal)
                     }
                 }
+                .padding(20)
             }
-            .ignoresSafeArea()
         }
+        .ignoresSafeArea()
+        
     }
 }
-    #Preview {
-        MealMainView()
-    }
+#Preview {
+    MealMainView()
+}
