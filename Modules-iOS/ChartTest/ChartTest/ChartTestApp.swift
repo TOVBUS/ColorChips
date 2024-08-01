e778939d6f988546d37559e45d8e4aa155946d1b
HyeS00 <hyesookim1118@gmail.com>
2024-08-01 09:49:34 +0900

    🎉 :: CharTestView 완셩 #KAN-263

---
+++ Modules-iOS/ChartTest/ChartTest/ChartTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartTestApp.swift
+//  ChartTest
+//
+//  Created by 김혜수 on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ChartTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            InbodyMainView(inbodys: inbodyDummyData)
+        }
+    }
+}
