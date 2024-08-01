cfc598a476b9fe7188c6bd543a1b555bfa7f24f2
DOYEON <debby_@kakao.com>
2024-08-01 14:12:06 +0900

    🎨 :: Add Login Button

---
+++ Frontend-iOS/FebirdApp/FebirdApp/Sources/Login/SocialLoginView.swift
@@ -0,0 +1,20 @@
+//
+//  SocialLoginView.swift
+//  FebirdApp
+//
+//  Created by doyeonjeong on 8/1/24.
+//
+
+import SwiftUI
+
+struct SocialLoginView: View {
+    var body: some View {
+        CustomButtonView(title: "Apple Sign In") {
+            // TODO: 로그인 로직 추가
+        }
+    }
+}
+
+#Preview {
+    SocialLoginView()
+}
