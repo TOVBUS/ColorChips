a5140a87c3161eae1eda74680b5a3ce2579a26b4
이유경 <a260@iyugyeong-ui-MacBookPro.local>
2024-08-04 06:13:49 +0900

    🎨 :: ExerciseGraduationView 뒷배경 일부 노출 조정 #KAN-277

--- Frontend-iOS/FebirdApp/FebirdApp/Sources/Exercise/Views/ExerciseGraduationView.swift
+++ Frontend-iOS/FebirdApp/FebirdApp/Sources/Exercise/Views/ExerciseGraduationView.swift
@@ -12,39 +12,27 @@ struct ExerciseGraduationView: View {
     @EnvironmentObject var navigationPathFinder: NavigationPathFinder<ExerciseViewOptions>
     
     var body: some View {
-        VStack {
-            Text("졸업을 축하한다!")
-                .font(.customFont(size: 22, weight: .bold))
-                .padding(.top, 80)
-            
-            ZStack {
+        ZStack {
+            Color.white.ignoresSafeArea()
+            VStack {
+                Text("졸업을 축하한다!")
+                    .font(.customFont(size: 22, weight: .bold))
+                    .foregroundStyle(.gray100)
+                    .padding(.top, 30)
+                
                 Image("feoPrincipal")
                     .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 330)
-                    .padding(.bottom, 230)
-                    .padding(.top, -50)
-                ZStack{
-                    Image("Diploma")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 284, height: 480)
-                        .padding(.bottom, -170)
+                    .scaledToFit()
+                    .frame(width: 380)
+                
+
+                
+                CustomButtonView(title: "이얏호~! 졸업이다~! 🎓", style: .orange){
+                    navigationPathFinder.popToRoot()
+                    tabViewModel.isHidden = false
                 }
             }
-            
-            Spacer()
-            
-            CustomButtonView(title: "이얏호~! 졸업이다~! 🎓", style: .orange){
-                navigationPathFinder.popToRoot()
-                tabViewModel.isHidden = false
-            }
         }
-        .background(
-            Rectangle()
-                .foregroundStyle(.white)
-        )
-        .ignoresSafeArea()
     }
 }
 
