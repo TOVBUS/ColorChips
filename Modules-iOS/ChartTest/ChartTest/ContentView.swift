baa9d828fe80bc44237737cc90ff881d10dab4dc
HyeS00 <hyesookim1118@gmail.com>
2024-08-04 01:01:40 +0900

    ✨ :: 데이터가 없을 경우 이미지 로직 구현 #KAN-263

--- Modules-iOS/ChartTest/ChartTest/ContentView.swift
+++ Modules-iOS/ChartTest/ChartTest/ContentView.swift
@@ -92,77 +92,84 @@ struct InbodyChartView<T: Plottable & BinaryFloatingPoint>: View {
                 Spacer()
             }
 // TODO:  - 정보가 없을 때 이미지
-            ScrollView(.horizontal, showsIndicators: false) {
-                ScrollViewReader { proxy in
-                    Chart {
-                        ForEach(filteredInbodys, id: \.inbodyDate) { inbody in
-                            if let value = getValue(inbody) {
-                                LineMark(
-                                    x: .value(xAxisTitle, inbody.inbodyDate),
-                                    y: .value(yAxisTitle, value)
-                                )
-                                .foregroundStyle(color)
+            
+            if filteredInbodys.isEmpty {
+                Image("EmptyInbodyGraph")
+                    .padding(.bottom, 50)
+            } else {
 
-                                PointMark(
-                                    x: .value(xAxisTitle, inbody.inbodyDate),
-                                    y: .value(yAxisTitle, value)
-                                )
-                                .foregroundStyle(color)
-                            }
-                        }
-                    }
-                    .frame(width: max(UIScreen.main.bounds.width - 120, CGFloat(filteredInbodys.count * 70)))
-                    .padding(.horizontal, 18)
-                    .chartXAxis {
-                        AxisMarks(values: filteredInbodys.map { $0.inbodyDate }) { value in
-                            if let date = value.as(Date.self) {
-                                AxisValueLabel {
-                                    Text(dateFormatter.string(from: date))
-                                        .font(.caption)
-                                }
-                            }
-                            AxisTick()
-                            AxisGridLine()
-                        }
-                    }
-                    .id(filteredInbodys.last!.inbodyDate)
-                    .chartXScale(domain: filteredInbodys.first!.inbodyDate...filteredInbodys.last!.inbodyDate.addingTimeInterval(2000000))
-                    .chartYScale(domain: minValue...maxValue)
-                    .onAppear(perform: {
-                        withAnimation {
-                            proxy.scrollTo(filteredInbodys.last!.inbodyDate, anchor: .trailing)
-                        }
-                    })
-                    .overlay(
-                        GeometryReader { geometry in
+                ScrollView(.horizontal, showsIndicators: false) {
+                    ScrollViewReader { proxy in
+                        Chart {
                             ForEach(filteredInbodys, id: \.inbodyDate) { inbody in
                                 if let value = getValue(inbody) {
-                                    Text(String(format: "%.1f", Double(value)))
-                                        .font(.caption)
-                                        .foregroundStyle(Color.gray)
-                                        .position(
-                                            x: xPosition(for: inbody.inbodyDate, in: geometry),
-                                            y: yPosition(for: value, in: geometry)
-                                        )
-                                        .offset(y: -25)
+                                    LineMark(
+                                        x: .value(xAxisTitle, inbody.inbodyDate),
+                                        y: .value(yAxisTitle, value)
+                                    )
+                                    .foregroundStyle(color)
+
+                                    PointMark(
+                                        x: .value(xAxisTitle, inbody.inbodyDate),
+                                        y: .value(yAxisTitle, value)
+                                    )
+                                    .foregroundStyle(color)
                                 }
                             }
                         }
-                    )
+                        .frame(width: max(UIScreen.main.bounds.width - 120, CGFloat(filteredInbodys.count * 70)))
+                        .padding(.horizontal, 18)
+                        .chartXAxis {
+                            AxisMarks(values: filteredInbodys.map { $0.inbodyDate }) { value in
+                                if let date = value.as(Date.self) {
+                                    AxisValueLabel {
+                                        Text(dateFormatter.string(from: date))
+                                            .font(.caption)
+                                    }
+                                }
+                                AxisTick()
+                                AxisGridLine()
+                            }
+                        }
+                        .id(filteredInbodys.last!.inbodyDate)
+                        .chartXScale(domain: filteredInbodys.first!.inbodyDate...filteredInbodys.last!.inbodyDate.addingTimeInterval(2000000))
+                        .chartYScale(domain: minValue...maxValue)
+                        .onAppear(perform: {
+                            withAnimation {
+                                proxy.scrollTo(filteredInbodys.last!.inbodyDate, anchor: .trailing)
+                            }
+                        })
+                        .overlay(
+                            GeometryReader { geometry in
+                                ForEach(filteredInbodys, id: \.inbodyDate) { inbody in
+                                    if let value = getValue(inbody) {
+                                        Text(String(format: "%.1f", Double(value)))
+                                            .font(.caption)
+                                            .foregroundStyle(Color.gray)
+                                            .position(
+                                                x: xPosition(for: inbody.inbodyDate, in: geometry),
+                                                y: yPosition(for: value, in: geometry)
+                                            )
+                                            .offset(y: -25)
+                                    }
+                                }
+                            }
+                        )
+                    }
                 }
-            }
-            .frame(height: 200)
-            .padding(.horizontal, 18)
-            .background(
-                RoundedRectangle(cornerRadius: 32)
-                    .fill(Color.gray.opacity(0.1))
-            )
-            .padding(.horizontal, 25)
+                .frame(height: 200)
+                .padding(.horizontal, 18)
+                .background(
+                    RoundedRectangle(cornerRadius: 32)
+                        .fill(Color.gray.opacity(0.1))
+                )
+                .padding(.horizontal, 25)
 
-            TrendAnalysisView(trendMessage: analyzeTrend())
-                .padding(.bottom, 50)
-                .padding(.top, 15)
-                .padding(.horizontal, 20)
+                TrendAnalysisView(trendMessage: analyzeTrend())
+                    .padding(.bottom, 50)
+                    .padding(.top, 15)
+                    .padding(.horizontal, 20)
+            }
         }
     }
 }
