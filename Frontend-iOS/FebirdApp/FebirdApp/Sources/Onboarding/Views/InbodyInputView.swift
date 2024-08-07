2aa0a979c569441b132760ce71b5e61ffd2aad98
HyeS00 <hyesookim1118@gmail.com>
2024-08-08 01:27:15 +0900

    🎨 :: 인바디 수기 입력 뷰 UI 수정 #KAN-322

--- Frontend-iOS/FebirdApp/FebirdApp/Sources/Onboarding/Views/InbodyInputView.swift
+++ Frontend-iOS/FebirdApp/FebirdApp/Sources/Onboarding/Views/InbodyInputView.swift
@@ -21,20 +21,23 @@ struct InbodyInputView: View {
         VStack {
             OnboardingGaugeView(progress: 5)
 
-            ScrollView {
-                Text("체중과 키를 기록하면 변화를 그래프로 보여드릴게요 😉")
-                    .font(.customFont(size: 20, weight: .bold))
-                    .foregroundStyle(Color(red: 0.07, green: 0.07, blue: 0.08))
-                    .padding(.bottom, 46)
+            VStack {
 
-                VStack(spacing: 20, content: {
-                    OnboardingTextField(question: "체중 *", placeholder: "46.3", unit: "kg", inputValue: nil, text: $weight)
-                    OnboardingTextField(question: "키 *", placeholder: "160.3", unit: "cm", inputValue: nil, text: $height)
-                    OnboardingTextField(question: "BMI", placeholder: "17.6", unit: "%", inputValue: nil, text: $bmi)
-                    OnboardingTextField(question: "체지방량", placeholder: "9.6", unit: "%", inputValue: nil, text: $bodyfat)
-                    OnboardingTextField(question: "기초대사량", placeholder: "kcal", unit: "kg", inputValue: nil, text: $bmr)
-                })
-                .padding(.horizontal, 46)
+                ScrollView {
+                    Text("체중과 키를 기록하면 변화를 그래프로 보여드릴게요 😉")
+                        .font(.customFont(size: 20, weight: .bold))
+                        .foregroundStyle(Color(red: 0.07, green: 0.07, blue: 0.08))
+                        .padding(.bottom, 46)
+
+                    VStack(spacing: 20, content: {
+                        OnboardingTextField(question: "체중 *", placeholder: "46.3", unit: "kg", inputValue: nil, text: $weight)
+                        OnboardingTextField(question: "키 *", placeholder: "160.3", unit: "cm", inputValue: nil, text: $height)
+                        OnboardingTextField(question: "BMI", placeholder: "17.6", unit: "%", inputValue: nil, text: $bmi)
+                        OnboardingTextField(question: "체지방량", placeholder: "9.6", unit: "%", inputValue: nil, text: $bodyfat)
+                        OnboardingTextField(question: "기초대사량", placeholder: "kcal", unit: "kg", inputValue: nil, text: $bmr)
+                    })
+                    .padding(.horizontal, 46)
+                }
 
                 CustomButtonView(title: "저장하기") {
                     // TODO: 데이터 저장 로직 구현 - SwiftData
