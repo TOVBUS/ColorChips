8e5e946d80fc1b43631cb3e6fb1ef886aa7c7120
uk10105 <lykalso33@naver.com>
2024-08-07 16:37:25 +0900

    🗑️ :: ExerciseListView 주석 제거 #KAN-316

--- Frontend-iOS/FebirdApp/FebirdApp/Sources/Exercise/Views/Components/ExerciseListView.swift
+++ Frontend-iOS/FebirdApp/FebirdApp/Sources/Exercise/Views/Components/ExerciseListView.swift
@@ -9,11 +9,6 @@ import SwiftUI
 
 struct ExerciseListView: View {
     @EnvironmentObject var navigationPathFinder: NavigationPathFinder<ExerciseViewOptions>
-//    @State private var exercises: [ExerciseItem] = [
-//        ExerciseItem(name: "스모 스쿼트", set: "3ea * 3set", isFinished: true, isStarted: false)
-//        ExerciseItem(name: "버터플라이 스윙", set: "3ea * 3set", isFinished: false, isStarted: true),
-//        ExerciseItem(name: "머리박박밀기", value: 3, set: 1, isFinished: false, isStarted: false)
-//    ]
     @State private var isStarted = false
 
     var body: some View {
